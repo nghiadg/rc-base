beca66f7d2316029d8c3234a800dff13216138cc
nghiadg <nghiadt.dev@gmail.com>
2023-08-26 23:00:02 +0700

    Base: Fix box shadow for AppDatePicker and AppSelect

--- src/components/common/AppSelect/AppSelect.tsx
+++ src/components/common/AppSelect/AppSelect.tsx
@@ -50,7 +50,7 @@ export const AppSelect = ({
       ...baseStyles,
       borderRadius: 0,
       margin: 0,
-      boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px;",
+      boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
       border: "1px solid var(--secondary-hover)",
     }),
     []
